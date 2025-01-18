1430a8ee6ea9902f9b3b845f3b2e5e1df9ae61db
maggy <perezmag5137@gmail.com>
2025-01-17 19:33:45 -0500

    created sign up functionalities

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,41 @@
+import React, { useState } from 'react';
+import { ref, set } from 'firebase/database';
+import { database } from './firebase'; // Import from your firebase.js file
+
 function Login(){
     // JS Logic
-
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
 
     /**
      * Function that switches window to the home page
      */
     function switchWindowAfterLogin(){
         window.location.href = "home.jsx";
+    }
+
+    /**
+     * Function that signs up the user
+     */
+    function signUpButton(e){
+
+        //Prevent the form from submitting
+        e.preventDefault();
+        
+        
+            const userId = new Date().getTime(); // Unique ID for the data
+            set(ref(database, `users/${userId}`), {
+            Username: Username,
+            Password: Password,
+            })
+            .then(() => {
+                alert('Data written successfully!');
+                setUsername('');
+                setPassword('');
+            })
+            .catch((error) => {
+                console.error('Error writing data: ', error);
+            });
     }
 
 
@@ -106,7 +135,7 @@ function Login(){
                         {/* Enter Username */}
                         <div>
                             <label htmlFor="Username" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Enter Username</label>
-                            <input type="text" name="username" id="username" class="
+                            <input type="text" name="username" id="username" className="
                             bg-violet-600 border border-violet-400 text-white text-sm rounded-lg focus:ring-violet-300 focus:border-violet-400 block w-full p-4 
                             dark:bg-violet-700 dark:border-violet-600 dark:placeholder-violet-400 dark:text-white dark:focus:ring-violet-300 dark:focus:border-violet-400" 
                             placeholder="Username" required/>
@@ -115,64 +144,65 @@ function Login(){
 
                         {/* Enter Password */}
                         <div>
-                        <label for="password" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Enter Password</label>
-                        <input type="password" name="password" id="password" placeholder="Password" class="bg-violet-600 border border-violet-400 text-white text-sm 
+                        <label htmlFor="password" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Enter Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password" className="bg-violet-600 border border-violet-400 text-white text-sm 
                         rounded-lg focus:ring-violet-300 focus:border-violet-400 block w-full p-4 dark:bg-violet-700 dark:border-violet-600 
                         dark:placeholder-violet-400 dark:text-white dark:focus:ring-violet-300 dark:focus:border-violet-400" required/>
                       </div>
 
 
                       {/* Login Button */}
-                      <button id="signin-button" type="submit" class="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none 
+                      <button id="signin-button" type="submit" className="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none 
                        focus:ring-violet-300 font-medium rounded-lg text-base px-5 py-4 text-center dark:bg-violet-700 dark:hover:bg-violet-800 
                        dark:focus:ring-violet-500">Sign in</button>
 
-                      <p class="text-white text-center" id="login-message"></p>
+                      <p className="text-white text-center" id="login-message"></p>
 
 
                       {/* Switch to Sign Up */}
-                      <p class="text-sm text-center font-light text-gray-500 dark:text-gray-400">
+                      <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">
                             Don’t have an account yet? <a href="#" onClick={(e) => switchToSignUp(e)} className="font-medium text-violet-600 hover:underline dark:text-violet-500">Sign up</a>
                        </p>
                     </form>
 
 
                     {/* Sign Up Form (New Users) */}
-                    <div id="signUp-field" class="hidden">
-                        <form action="" class="space-y-6">
-                          <h1 class="text-3xl font-bold text-center">Create an account!</h1>
+                    <div id="signUp-field" className="hidden">
+                        <form onSubmit={signUpButton} className="space-y-6">
+                          <h1 className="text-3xl font-bold text-center">Create an account!</h1>
 
                           {/* Create Username */}
                             <div>
-                              <label for="username" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Create Username</label>
+                              <label htmlFor="username" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Create Username</label>
                               <input type="text" name="username" id="create-username-field" placeholder="Username" 
-                              class="
+                              className="
                             bg-violet-600 border border-violet-400 text-white text-sm rounded-lg focus:ring-violet-300 focus:border-violet-400 block w-full p-4 
                             dark:bg-violet-700 dark:border-violet-600 dark:placeholder-violet-400 dark:text-white dark:focus:ring-violet-300 dark:focus:border-violet-400" 
-                            required />
+                            value={Username} onChange={(e) => setUsername(e.target.value)} required />
                             </div>
 
 
                             {/* Create Password */}
                              <div>
-                              <label for="password" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">Create Password</label>
+                              <label htmlFor="password" className="block mb-2 text-base font-medium text-gray-900 dark:text-white">Create Password</label>
                               <input type="password" name="password" id="create-password-field" placeholder="Password" 
-                              class="bg-violet-600 border border-violet-400 text-white text-sm 
+                              className="bg-violet-600 border border-violet-400 text-white text-sm 
                               rounded-lg focus:ring-violet-300 focus:border-violet-400 block w-full p-4 dark:bg-violet-700 dark:border-violet-600 
-                              dark:placeholder-violet-400 dark:text-white dark:focus:ring-violet-300 dark:focus:border-violet-400" required />
+                              dark:placeholder-violet-400 dark:text-white dark:focus:ring-violet-300 dark:focus:border-violet-400" 
+                              value={Password} onChange={(e) => setPassword(e.target.value)} required />
                              </div>
 
 
                              {/* Sign Up Button */}
-                             <button id="signup-button" type="submit" class="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none 
+                             <button id="signup-button" type="submit" className="w-full text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 focus:outline-none 
                              focus:ring-violet-300 font-medium rounded-lg text-base px-5 py-4 text-center dark:bg-violet-700 dark:hover:bg-violet-800 
                              dark:focus:ring-violet-500">Sign up</button>
       
-                             <p class="text-white text-center" id="login-message"></p>
+                             <p className="text-white text-center" id="login-message"></p>
       
       
                              {/* Switch to Sign In */}
-                             <p class="text-sm text-center font-light text-gray-500 dark:text-gray-400">
+                             <p className="text-sm text-center font-light text-gray-500 dark:text-gray-400">
                               Already signed up? <a href="#" onClick={(e) => switchToLogin(e)} className="font-medium text-violet-600 hover:underline dark:text-violet-500">Sign in</a>
                              </p>
                         </form>
