2db6c2464d759aa44990213e81ef22cc48b63b20
maggy <perezmag5137@gmail.com>
2025-02-27 19:04:19 -0500

    stockroom layout

--- src/pages/Stockroom.jsx
+++ src/pages/Stockroom.jsx
@@ -2,15 +2,24 @@ import { Link } from "react-router-dom";
 
 import Sidebar from "../components/Sidebar";
 import PageHeader from "../components/PageHeader";
+import DarkModeToggle from "../components/DarkModeToggle";
 
 
 function Stockroom() {
 
     return (
-        <main className="lg:pl-56 lg:duration-300">
+        <main className="lg:pl-56 lg:duration-300 text-black dark:text-white">
             <Sidebar />
 
-            <PageHeader title="Stockroom" />
+            <div className="flex justify-between">
+                <PageHeader title="Stockroom" />
+                <DarkModeToggle />
+
+            </div>
+
+            <div>
+
+            </div>
         </main>
     );
 }
