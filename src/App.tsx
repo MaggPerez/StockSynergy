d6af062bc670f833c684d77e6d3b4b918b52490b
maggy <perezmag5137@gmail.com>
2025-06-13 12:56:34 -0400

    testing supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Login from "./pages/Login.js";
 import Home from "./pages/Home.js"
+import SupaLogic from "./components/supaLogic.jsx";
 
 function App() {
 
   return (
     <>
-    <Login/>
+    <SupaLogic />
     </>
   );
 }
