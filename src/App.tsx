b94dcd8dbfc10ce221a075c0760bcb4f106ce967
maggy <perezmag5137@gmail.com>
2025-06-22 13:45:12 -0400

    removed import statements that are no longer in used

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import Login from "./pages/Login.js";
 import Home from "./pages/Home.js"
-import SupaLogic from "./components/SupaLogic.jsx";
-import ProductItems from "./components/ProductItems.js";
 
 function App() {
 
   return (
     <>
-    {/* <SupaLogic /> */}
     <Login />
     </>
   );
