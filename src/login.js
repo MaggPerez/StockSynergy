4eb59387ca7647d9a681f6aec3fb65480dc0b465
MaggPerez <perezmag5137@gmail.com>
2025-09-05 00:23:36 -0400

    integrated a more mature firebase signup/signin

--- src/login.js
+++ src/login.js
@@ -1,4 +1,6 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getDoc, doc, setDoc } from "firebase/firestore";
+import { firestoreDB } from "./firebase";
 
 /**
  * Sign in function
@@ -13,8 +15,8 @@ export async function signIn(email, password) {
             const user = userCredential.user;
             console.log("Sign in worked")
 
-            sessionStorage.setItem("isManager", true)
-            sessionStorage.setItem("username", user.email);
+            //Checking if the user is a manager
+            isManager(user)
 
             document.getElementById('login-message').innerHTML = "Logging in! Loading...";
             document.getElementById('login-message').style.color = "#00FF7F";
@@ -39,6 +41,9 @@ export async function signUp(email, password) {
             const user = userCredential.user;
             console.log("user created")
 
+            //Setting up user's profile
+            setUpUserProfile(user)
+
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -46,3 +51,38 @@ export async function signUp(email, password) {
             console.log(errorCode, errorMessage)
         });
 }
+
+/**
+ * Function that sets up user's profile
+ * @param {import('firebase/auth').User} user - Firebase user object
+ */
+async function setUpUserProfile(user) {
+    try {
+        await setDoc(doc(firestoreDB, "Users", user.uid), {
+            email: user.email,
+            name: user.name || "",
+            createdAt: new Date(),
+            isManager: false
+        })
+    } catch (error) {
+        console.log(error.code, error.message)
+    }
+}
+
+/**
+ * 
+ * * @param {import('firebase/auth').User} user - Firebase user object
+ */
+async function isManager(user) {
+    const docRef = doc(firestoreDB, "Users", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists() && docSnap.data().isManager) {
+        sessionStorage.setItem("isManager", docSnap.data().isManager)
+        sessionStorage.setItem("username", docSnap.data().email);
+    }
+    else {
+        
+    }
+}
+
